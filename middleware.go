106dbb50dc94daab5fffe002cfc6ca97019d2d54
Cristian Dean <cristian.dean@corp.globo.com>
2019-09-25 00:02:30 -0300

    WIP

--- middleware.go
+++ middleware.go
@@ -1,45 +1,47 @@
-package api
+package echoprometheus
 
 import (
+	"fmt"
 	"reflect"
 
 	echo "github.com/labstack/echo/v4"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-
 )
 
-var (
-	httpRequests = promauto.NewCounterVec(prometheus.CounterOpts{
-		Namespace: "echo",
-		Subsystem: "http",
-		Name:      "requests_total",
-		Help:      "Number of HTTP operations",
-	}, []string{"status", "method", "handler"})
+// Config responsible to configure middleware
+type Config struct {
+	Namespace string
+	Buckets   []float64
+	Subsystem string
+}
 
-	httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "echo",
-		Subsystem: "http",
-		Name:      "request_duration_seconds",
-		Help:      "Spend time by processing a route",
-		Buckets: []float64{
-			0.0005,
-			0.001, // 1ms
-			0.002,
-			0.005,
-			0.01, // 10ms
-			0.02,
-			0.05,
-			0.1, // 100 ms
-			0.2,
-			0.5,
-			1.0, // 1s
-			2.0,
-			5.0,
-		},
-	}, []string{"method", "handler"})
+const (
+	httpRequestsCount    = "requests_total"
+	httpRequestsDuration = "request_duration_seconds"
+	notFoundPath         = "/not-found"
 )
 
+// DefaultConfig has the default instrumentation config
+var DefaultConfig = Config{
+	Namespace: "echo",
+	Subsystem: "http",
+	Buckets: []float64{
+		0.0005,
+		0.001, // 1ms
+		0.002,
+		0.005,
+		0.01, // 10ms
+		0.02,
+		0.05,
+		0.1, // 100 ms
+		0.2,
+		0.5,
+		1.0, // 1s
+		2.0,
+		5.0,
+	},
+}
 
 func normalizeHTTPStatus(status int) string {
 	if status < 200 {
@@ -54,32 +56,61 @@ func normalizeHTTPStatus(status int) string {
 	return "5xx"
 }
 
-
 func isNotFoundHandler(handler echo.HandlerFunc) bool {
 	return reflect.ValueOf(handler).Pointer() == reflect.ValueOf(echo.NotFoundHandler).Pointer()
 }
 
-func MetricsMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		req := c.Request()
-		path := c.Path()
+// NewConfig returns a new config with default values
+func NewConfig() Config {
+	return DefaultConfig
+}
 
-		// to avoid attack high cardinality of 404
-		if isNotFoundHandler(c.Handler()) {
-			path = "/not-found"
+// MetricsMiddlewareDefault returns an echo middleware with default config for instrumentation.
+func MetricsMiddlewareDefault() echo.MiddlewareFunc {
+	return MetricsMiddleware(DefaultConfig)
+}
+
+// MetricsMiddleware returns an echo middleware for instrumentation.
+func MetricsMiddleware(config Config) echo.MiddlewareFunc {
+
+	httpRequests := promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: config.Namespace,
+		Subsystem: config.Subsystem,
+		Name:      httpRequestsCount,
+		Help:      "Number of HTTP operations",
+	}, []string{"status", "method", "handler"})
+
+	httpDuration := promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: config.Namespace,
+		Subsystem: config.Subsystem,
+		Name:      httpRequestsDuration,
+		Help:      "Spend time by processing a route",
+		Buckets:   config.Buckets,
+	}, []string{"method", "handler"})
+	fmt.Println("-------------------")
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			path := c.Path()
+
+			// to avoid attack high cardinality of 404
+			if isNotFoundHandler(c.Handler()) {
+				path = notFoundPath
+			}
+
+			timer := prometheus.NewTimer(httpDuration.WithLabelValues(req.Method, path))
+			err := next(c)
+			timer.ObserveDuration()
+
+			if err != nil {
+				c.Error(err)
+			}
+
+			status := normalizeHTTPStatus(c.Response().Status)
+			httpRequests.WithLabelValues(status, req.Method, path).Inc()
+
+			return err
 		}
-
-		timer := prometheus.NewTimer(httpDuration.WithLabelValues(req.Method, path))
-		err := next(c)
-		timer.ObserveDuration()
-
-		if err != nil {
-			c.Error(err)
-		}
-
-		status := normalizeHTTPStatus(c.Response().Status)
-		httpRequests.WithLabelValues(status, req.Method, path).Inc()
-
-		return err
 	}
 }
